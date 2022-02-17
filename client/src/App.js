53f977beac5090074beca1efffd3c49fe984f35c
ByeongGwon <lee971009@naver.com>
2022-02-17 19:39:38 +0900

    1

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import "./App.css";
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import OtherPage from "./OtherPage";
 import MainComponent from "./MainComponent";
+import MapContainer from "./mapContainer";
 
 function App() {
   return (
@@ -10,12 +11,17 @@ function App() {
       <Fragment>
         <header className="header">
           <div> React Node postgreSQL Connection </div>
+
           <Link to="/">Home</Link>
           <Link to="/otherpage">Other page[아무것도 없음 연동 확인용]</Link>
+          <Link to="/map">Map</Link>
         </header>
         <div className="main">
           <Route exact path="/" component={MainComponent} />
           <Route path="/otherpage" component={OtherPage} />
+          <Route path="/map" component={MapContainer} />
+
+          <MapContainer />
         </div>
       </Fragment>
     </Router>
---
+++ client/src/mapContainer.js
@@ -0,0 +1,34 @@
+/*global kakao */
+import React, { useEffect } from "react";
+import { department } from "./department";
+
+export default function Map() {
+  useEffect(() => {
+    mapscript();
+  }, []);
+
+  const mapscript = () => {
+    let container = document.getElementById("map");
+    let options = {
+      center: new kakao.maps.LatLng(37.29742255286631, 126.8355379155881),
+      level: 4,
+    };
+
+    //map
+    const map = new kakao.maps.Map(container, options);
+
+    department.forEach((el) => {
+      // 마커를 생성합니다
+      new kakao.maps.Marker({
+        //마커가 표시 될 지도
+        map: map,
+        //마커가 표시 될 위치
+        position: new kakao.maps.LatLng(el.lat, el.lng),
+        //마커에 hover시 나타날 title
+        title: el.title,
+      });
+    });
+  };
+
+  return <div id="map" style={{ width: "50vw", height: "50vh" }}></div>;
+}
