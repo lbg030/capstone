37405a488e11cc929e7419e7e9b87f8fef0d8bd4
HONGVELOPER <hyjinn1003@naver.com>
2022-03-09 13:44:54 +0900

    react node k8s setting end

--- client/src/MainComponent.js
+++ client/src/MainComponent.js
@@ -3,55 +3,76 @@ import axios from "axios";
 import "./MainComponent.css";
 
 const MainComponent = () => {
-  const [values, setValues] = useState([]);
-  const [value, setValue] = useState("");
+	const [values, setValues] = useState([]);
+	const [value, setValue] = useState("");
 
-  const getAllNumbers = useCallback(async () => {
-    // we will use nginx to redirect it to the proper URL
-    const data = await axios.get("/api/values/all");
-    setValues(data.data.rows.map(row => row.number));
-  }, []);
+	const getAllNumbers = useCallback(async () => {
+		// we will use nginx to redirect it to the proper URL
+		const data = await axios.get("/api/values/all");
+		setValues(data.data.rows.map((row) => row.number));
+	}, []);
 
-  const saveNumber = useCallback(
-    async event => {
-      event.preventDefault();
+	const saveNumber = useCallback(
+		async (event) => {
+			event.preventDefault();
 
-      await axios.post("/api/values", {
-        value
-      });
+			await axios.post("/api/values", {
+				value,
+			});
 
-      setValue("");
-      getAllNumbers();
-    },
-    [value, getAllNumbers]
-  );
+			setValue("");
+			getAllNumbers();
+		},
+		[value, getAllNumbers]
+	);
 
-  useEffect(() => {
-    getAllNumbers();
-  }, []);
+	const openClick = async () => {
+		try {
+			const result = await axios.post("/function/nodeexpress", {
+				headers: {
+					"Content-Type": "text/plain",
+				},
+				body: "test",
+			});
+			console.log(result, "openfass api result");
+		} catch (error) {
+			console.log(error);
+		}
+	};
 
-  return (
-    <div>
-      <button onClick={getAllNumbers}>Get all numbers</button>
-      <br />
-      <span className="title">Values</span>
-      <div className="values">
-        {values.map(value => (
-          <div className="value">{value}</div>
-        ))}
-      </div>
-      <form className="form" onSubmit={saveNumber}>
-        <label>Enter your value: </label>
-        <input
-          value={value}
-          onChange={event => {
-            setValue(event.target.value);
-          }}
-        />
-        <button>Submit</button>
-      </form>
-    </div>
-  );
+	useEffect(() => {
+		getAllNumbers();
+	}, []);
+
+	return (
+		<div>
+			<button onClick={getAllNumbers}>Get all numbers</button>
+			<br />
+			<span className="title">Values</span>
+			<div className="values">
+				{values.map((value) => (
+					<div className="value">{value}</div>
+				))}
+			</div>
+			<form className="form" onSubmit={saveNumber}>
+				<label>Enter your value: </label>
+				<input
+					value={value}
+					onChange={(event) => {
+						setValue(event.target.value);
+					}}
+				/>
+				<button>Submit</button>
+			</form>
+			<h1>Kubernetes Test</h1>
+			<div className="Wrapper">
+				<h3>Hello from OPENFAAS</h3>
+				<div className="Button" onClick={openClick}>
+					Click
+				</div>
+			</div>
+		</div>
+	);
 };
 
 export default MainComponent;
