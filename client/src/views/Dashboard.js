a2a3d656db48b1fbdd3689dcae70a48865672654
Kimhannah <skqkdldhf98@naver.com>
2022-03-10 17:42:13 +0900

    [client] setState로 전체 데이터 가공중. chartist 시각화 임시데이터로 테스트 완료

--- client/src/views/Dashboard.js
+++ client/src/views/Dashboard.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import ChartistGraph from "react-chartist";
 // react-bootstrap components
 import {
@@ -18,21 +18,37 @@ import {
 import axios from "axios";
 
 function Dashboard() {
-  // usestate useEffect로 비동기 처리 -> 렌더링 전에 데이터 받기 위해!
-  const data = axios
-    .get("http://localhost:3001/backend/patient/age")
-    .then((res) => {
-      console.log("client : data 받음");
-      console.log(res.data);
-    })
-    .catch((err) => {
+  // state object로 관리 -> 나중에 객체 수정하기
+  const [state, setState] = useState([]); // 전체 데이터 배열로 저장
+  async function ApiCall() {
+    try {
+      const obj = await axios.get("http://localhost:3001/backend/patient/age");
+      const array = obj.data;
+      console.log(array); // [{...}. {...}, {...}]
+      console.log(array[0]); // {id:1, age:20}
+
+      // state에 array 배열을 담고 싶음..!  => 근데 state가 object, object로 출력됨.. WHY?
+      // console.log(`before state ${state}`);
+      setState((state) => [...state, array]);
+      // console.log(state);
+    } catch (err) {
       console.log(err);
-    });
+    }
+  }
+  // usestate useEffect로 비동기 처리 -> 렌더링 전에 데이터 받기 위해!
+  useEffect(() => {
+    ApiCall();
+  }, []);
 
-  //graphql : 프론트에서 쿼리 요청
+  //graphql : 프론트에서 쿼리 요청 -> 백에서 처리 (지금은 사용 X)
   // filter, reduce -> 배열 만들기
-
-  // console.log(Object.keys(data).length); -> 총 데이터
+  // 임시 데이터 for chartist 테스트
+  const tmp = [
+    { id: 1, age: 20 },
+    { id: 2, age: 30 },
+    { id: 3, age: 40 },
+    { id: 4, age: 10 },
+  ];
 
   return (
     <>
@@ -41,7 +57,7 @@ function Dashboard() {
           <Col md="8">
             <Card>
               <Card.Header>
-                <Card.Title as="h4">Users Behavior</Card.Title>
+                <Card.Title as="h4">확진자 연령별 현황</Card.Title>
                 <p className="card-category">24 Hours performance</p>
               </Card.Header>
               <Card.Body>
@@ -49,25 +65,30 @@ function Dashboard() {
                   <ChartistGraph
                     data={{
                       labels: [
-                        "9:00AM",
-                        "12:00AM",
-                        "3:00PM",
-                        "6:00PM",
-                        "9:00PM",
-                        "12:00PM",
-                        "3:00AM",
-                        "6:00AM",
+                        "10대",
+                        "20대",
+                        "30대",
+                        "40대",
+                        // "12:00AM",
+                        // "3:00PM",
+                        // "6:00PM",
+                        // "9:00PM",
+                        // "12:00PM",
+                        // "3:00AM",
+                        // "6:00AM",
                       ],
                       series: [
-                        [287, 385, 490, 492, 554, 586, 698, 695],
-                        [67, 152, 143, 240, 287, 335, 435, 437],
-                        [23, 113, 67, 108, 190, 239, 307, 308],
+                        [tmp[0].id, tmp[1].id, tmp[2].id, tmp[3].id],
+                        // [287, 385, 490, 492, 554, 586, 698, 695],
+                        // [67, 152, 143, 240, 287, 335, 435, 437],
+                        // [23, 113, 67, 108, 190, 239, 307, 308],
                       ],
                     }}
                     type="Line"
                     options={{
                       low: 0,
-                      high: 800,
+                      high: 10,
+                      // high: 800,
                       showArea: false,
                       height: "245px",
                       axisX: {
@@ -114,7 +135,7 @@ function Dashboard() {
           <Col md="4">
             <Card>
               <Card.Header>
-                <Card.Title as="h4">Email Statistics</Card.Title>
+                <Card.Title as="h4">확진자 연령별 현황</Card.Title>
                 <p className="card-category">Last Campaign Performance</p>
               </Card.Header>
               <Card.Body>
