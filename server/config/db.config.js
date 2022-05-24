de54d009adb0bb41ee1f9d00475fb8d71c901c1b
ByeongGwon <lee971009@naver.com>
2022-05-24 23:08:00 +0900

    apply쪽 DB삭제 및 db.config IP추가

--- server/config/db.config.js
+++ server/config/db.config.js
@@ -1,11 +1,20 @@
 // const keys = require("../keys");
 
 module.exports = {
+<<<<<<< Updated upstream
   host: process.env.PGHOST,
   user: process.env.PGUSER,
   password: process.env.PGPASSWORD,
   database: process.env.PGDATABASE,
   port: process.env.PGPORT,
+=======
+  // host: "192.168.50.139", // 창준이형 IP
+  host: "192.168.2.5", //이병권 IP
+  user: "postgres",
+  password: "postgres",
+  database: "postgres",
+  port: "5432",
+>>>>>>> Stashed changes
   dialect: "postgres",
   pool: {
     max: 5,
